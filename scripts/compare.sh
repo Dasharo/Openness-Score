9f771e665aa2529e287d75c9ef95c054e00b4bc5
Pawel Langowski <pawel.langowski@3mdeb.com>
2025-07-03 13:40:19 +0200

    scripts/compare.sh: Add -a argument
    Signed-off-by: Pawel Langowski <pawel.langowski@3mdeb.com>

--- scripts/compare.sh
+++ scripts/compare.sh
@@ -49,25 +49,25 @@ download_protectli() {
 
 compare_protectli() {
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_all_fw6_vault_kbl_v1.0.14.rom" \
-    -c "$BINDIR/protectli/fw6_all_YKR6LV30.bin" -p "Protectli FW6"
+    -c "$BINDIR/protectli/fw6_all_YKR6LV30.bin" -p "Protectli FW6" -a "sklkbl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_v1210_v0.9.3.rom" \
-    -c "$BINDIR/protectli/v1210_JPL.2LAN.S4G.PCIE.6W.013.bin" -p "Protectli V1210"
+    -c "$BINDIR/protectli/v1210_JPL.2LAN.S4G.PCIE.6W.013.bin" -p "Protectli V1210" -a "jsl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_v1211_v0.9.3.rom" \
-    -c "$BINDIR/protectli/v1211_JPL.2LAN.D8G.PCIE.6W.009.bin" -p "Protectli V1211"
+    -c "$BINDIR/protectli/v1211_JPL.2LAN.D8G.PCIE.6W.009.bin" -p "Protectli V1211" -a "jsl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_v1410_v0.9.3.rom" \
-    -c "$BINDIR/protectli/v1410_JPL.4LAN.S8GB.PCIE.6W.007B.bin" -p "Protectli V1410"
+    -c "$BINDIR/protectli/v1410_JPL.4LAN.S8GB.PCIE.6W.007B.bin" -p "Protectli V1410" -a "jsl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_v1610_v0.9.3.rom" \
-    -c "$BINDIR/protectli/v1610_JPL.6LAN.D16G.PCIE.007.bin" -p "Protectli V1610"
+    -c "$BINDIR/protectli/v1610_JPL.6LAN.D16G.PCIE.007.bin" -p "Protectli V1610" -a "jsl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_vp2410_v1.1.1.rom" \
-    -c "$BINDIR/protectli/vp2410_GLK4L280.bin" -p "Protectli VP2410"
+    -c "$BINDIR/protectli/vp2410_GLK4L280.bin" -p "Protectli VP2410" -a "glk"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_vp2420_v1.2.1.rom" \
-    -c "$BINDIR/protectli/vp2420_YELD4L13P.bin" -p "Protectli VP2420"
+    -c "$BINDIR/protectli/vp2420_YELD4L13P.bin" -p "Protectli VP2420" -a "ehl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_vp2430_v0.9.0.rom" \
-    -c "$BINDIR/protectli/vp2430_PRALNDZ4L10.bin" -p "Protectli VP2430"
+    -c "$BINDIR/protectli/vp2430_PRALNDZ4L10.bin" -p "Protectli VP2430" -a "adl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_vp4600_v1.2.0.rom" \
-    -c "$BINDIR/protectli/vp4630_v2_YW6L2318.bin" -p "Protectli VP4600"
+    -c "$BINDIR/protectli/vp4630_v2_YW6L2318.bin" -p "Protectli VP4600" -a "cnl"
   ./openness_score/openness_score.py "$BINDIR/protectli/protectli_vp6600_v0.9.2.rom" \
-    -c "$BINDIR/protectli/vp6630_ADZ6L314.bin" -p "Protectli VP6600"
+    -c "$BINDIR/protectli/vp6630_ADZ6L314.bin" -p "Protectli VP6600" -a "adl"
 }
 
 download_msi() {
@@ -103,10 +103,10 @@ download_msi() {
 }
 
 compare_msi() {
-  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7d25_v1.1.4_ddr4.rom" -c "$BINDIR/msi/E7D25IMS.1L0" -p "MS-7D25 DDR4"
-  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7d25_v1.1.4_ddr5.rom" -c "$BINDIR/msi/E7D25IMS.AL0" -p "MS-7D25 DDR5"
-  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7e06_v0.9.2_ddr4.rom" -c "$BINDIR/msi/E7E06IMS.1F0" -p "MS-7E06 DDR4"
-  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7e06_v0.9.2_ddr4.rom" -c "$BINDIR/msi/E7E06IMS.AH0" -p "MS-7E06 DDR5"
+  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7d25_v1.1.4_ddr4.rom" -c "$BINDIR/msi/E7D25IMS.1L0" -p "MS-7D25 DDR4" -a "adl"
+  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7d25_v1.1.4_ddr5.rom" -c "$BINDIR/msi/E7D25IMS.AL0" -p "MS-7D25 DDR5" -a "adl"
+  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7e06_v0.9.2_ddr4.rom" -c "$BINDIR/msi/E7E06IMS.1F0" -p "MS-7E06 DDR4" -a "adl"
+  ./openness_score/openness_score.py "$BINDIR/msi/msi_ms7e06_v0.9.2_ddr4.rom" -c "$BINDIR/msi/E7E06IMS.AH0" -p "MS-7E06 DDR5" -a "adl"
 }
 
 download_novacustom() {
@@ -150,12 +150,12 @@ download_novacustom() {
 }
 
 compare_novacustom() {
-  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_v54x_mtl_v0.9.0.rom" -c "$BINDIR/novacustom/V5xxTU(32M).09" -p "NC V540TU"
-  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_v56x_mtl_v0.9.0.rom" -c "$BINDIR/novacustom/V5xxTU(32M).09" -p "NC V560TU"
-  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_nv4x_adl_v1.7.2_full.rom" -c "$BINDIR/novacustom/NV4xPZ(32M).03" -p "NC NV4xPZ"
-  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_nv4x_tgl_v1.5.2.rom" -c "$BINDIR/novacustom/NV4XBX.05" -p "NC NV4xME_MB"
-  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_ns5x_adl_v1.7.2.rom" -c "$BINDIR/novacustom/NSx0PU(32M).09" -p "NC NSxxPU"
-  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_ns5x_tgl_v1.5.2.rom" -c "$BINDIR/novacustom/NS50_70MU.16N" -p "NC NS5xMU"
+  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_v54x_mtl_v0.9.0.rom" -c "$BINDIR/novacustom/V5xxTU(32M).09" -p "NC V540TU" -a "mtl"
+  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_v56x_mtl_v0.9.0.rom" -c "$BINDIR/novacustom/V5xxTU(32M).09" -p "NC V560TU" -a "mtl"
+  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_nv4x_adl_v1.7.2_full.rom" -c "$BINDIR/novacustom/NV4xPZ(32M).03" -p "NC NV4xPZ" -a "adl"
+  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_nv4x_tgl_v1.5.2.rom" -c "$BINDIR/novacustom/NV4XBX.05" -p "NC NV4xME_MB" -a "tgl"
+  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_ns5x_adl_v1.7.2.rom" -c "$BINDIR/novacustom/NSx0PU(32M).09" -p "NC NSxxPU" -a "adl"
+  ./openness_score/openness_score.py "$BINDIR/novacustom/novacustom_ns5x_tgl_v1.5.2.rom" -c "$BINDIR/novacustom/NS50_70MU.16N" -p "NC NS5xMU" -a "tgl"
 }
 
 download_odroid() {
@@ -169,7 +169,7 @@ download_odroid() {
 }
 
 compare_odroid() {
-  ./openness_score/openness_score.py "$BINDIR/odroid/hardkernel_odroid_h4_v0.9.0.rom" -c "$BINDIR/odroid/ADLN-H4_B1.07.bin" -p "Odroid H4"
+  ./openness_score/openness_score.py "$BINDIR/odroid/hardkernel_odroid_h4_v0.9.0.rom" -c "$BINDIR/odroid/ADLN-H4_B1.07.bin" -p "Odroid H4" -a "adl"
 }
 
 if [ $# -lt 2 ]; then
